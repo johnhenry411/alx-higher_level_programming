8f863df9f1059bd41a5c414921ff9daee9ed5f89
John henry <henry@Henry.johnhenry.com>
2023-09-08 11:49:29 +0300

    third_day

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,17 @@
+ 
 #!/usr/bin/python3
+import sys
 
-if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    import sys
+if __name__ != "__main__":
+    exit()
 
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+argc = len(sys.argv) - 1
 
+i = 0
+result = 0
+for arg in sys.argv:
+    if i != 0:
+        result += int(arg)
+    else:
+        i += 1
+print("{:d}".format(result))
